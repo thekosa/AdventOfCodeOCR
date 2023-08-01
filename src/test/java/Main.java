7442b95bd9f0121eb41c562c3e5f5dc7abd992a8
Michał Kosieradzki <49490562+thekosa@users.noreply.github.com>
2023-08-01 16:11:03 +0200

    Included Main.java to build in test section

---
+++ src/test/java/Main.java
@@ -0,0 +1,14 @@
+import com.AdventOfCode.primitive.OCR.OCR;
+
+public class Main {
+    public static void main(String[] args) {
+        String string =
+                        "████ ████ ████  ██  █  █   ██  ██  ███  \n" +
+                        "█       █ █    █  █ █  █    █ █  █ █  █ \n" +
+                        "███    █  ███  █    ████    █ █  █ ███  \n" +
+                        "█     █   █    █    █  █    █ ████ █  █ \n" +
+                        "█    █    █    █  █ █  █ █  █ █  █ █  █ \n" +
+                        "████ ████ █     ██  █  █  ██  █  █ ███  ";
+        System.out.println(OCR.convert(string, " ", "█"));
+    }
+}
